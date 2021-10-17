52b38e8d7104d877dc40a980b6b1016d715c9c0b
andrewburich <37350955+andrewburich@users.noreply.github.com>
2021-10-16 21:44:48 -0400

    Update data_output_start_end.py

--- data_output_start_end.py
+++ data_output_start_end.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+x#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct  7 09:24:27 2021
@@ -37,7 +37,8 @@ def function(data, output, beginning, end):
 	orig_found=0
 	rc_found=0
 
-	len_seqs = []
+	len_seqs = [] #make this a touple containing the filename as the other item
+	len_seq_fnames = []
 
 	for i in file_list:
 	  #### "i" looks like: "Left_v12_HG00171_hgsvc_pbsq2-clr_1000-flye.h1-un.arrow-p1.fasta" ####
@@ -61,6 +62,7 @@ def function(data, output, beginning, end):
 	  if a.find(beginning) != -1 and a.find(end) != -1:
 		  sub_section = a[a.find(beginning)+len(beginning):a.rfind(end)]
 		  len_seqs.append(len(sub_section))
+		  len_seq_fnames.append(i)
 	  # Add beginning and end strings to the selected region
 		  out = beginning + sub_section + end
 	  
@@ -84,6 +86,7 @@ def function(data, output, beginning, end):
 			  rc_sub_section = rc_genome[rc_genome.find(beginning)+len(beginning):rc_genome.rfind(end)]
 			  rc_sub_section = str(rc_sub_section)
 			  len_seqs.append(len(rc_sub_section))
+			  len_seq_fnames.append(i)
 			  rc_out = beginning + rc_sub_section + end
 	  # Re-insert first line of fasta file for output
 			  rc_combined = firstLine + rc_out
@@ -111,6 +114,7 @@ def function(data, output, beginning, end):
 
 
 
+	file_len_tuple = list(zip(len_seqs,len_seq_fnames))
 	num_files = len(file_list)
 	percent_found = (num_found/num_files) * 100
 	percent_found = "{:.1f}".format(percent_found)
@@ -120,11 +124,15 @@ def function(data, output, beginning, end):
 	percent_rc_found = "{:.1f}".format(percent_rc_found)
 
 	shortest_seq = min(len_seqs)
+	short_sq_index = len_seqs.index(shortest_seq)
 	longest_seq = max(len_seqs)
+	long_sq_index = len_segs.index(longest_seq)
 	avg_seq = sum(len_seqs)/len(len_seqs)
 	avg_seq = "{:.0f}".format(avg_seq)
 
 	b_down = 'Break down of data:\n' + str(num_files) + ' files entered.\nThe start and end sequences or their reverse complements were found in ' + str(percent_found) + '% of the files.\n' + str(percent_original_found) + '% contained the original start and end sequences.\n' + str(percent_rc_found) + '% contained the reverse complement of the sequences.\nThe average length of the subsections found is ' + str(avg_seq) + ' characters\nThe shortest subsection found is ' + str(shortest_seq) + ' characters long, and the longest found is ' + str(longest_seq) + ' characters long.'                         
+	b_down += '\nFile containing the shortest sequence: ' + str(file_len_tuple[short_sq_index][1]) + '\nFile containing the longest sequence: ' + str(file_len_tuple[long_sq_index][1])
+
 	if len(not_found_list)>0:
 		b_down+= '\n\n\nFiles not containing original sequences or their reverse complements:'
 		for i in not_found_list:
